f2e5283af1478c56277bc8edc91a7ea0f996e070
metastable-void <git@yuka.h09.eu>
2023-10-29 07:41:04 +0900

    wip

--- webpack-src/storage/storage.ts
+++ webpack-src/storage/storage.ts
@@ -1,11 +1,55 @@
 
 import * as idb from './idb';
+import * as utf8 from '../utf8';
 import { HmacSha256 } from '../crypto/sha256';
 import { AesKey, AesMessage } from '../crypto/aes-gcm';
 
 
 export type KeyValueStoreName = string;
 export type KeyValueStoreKey = number | string;
+
+namespace TupleEncoder {
+    export type Value = string | number;
+
+    const TYPE_STRING = 1;
+    const TYPE_NUMBER = 2;
+
+    const concat = (a: Uint8Array, b: Uint8Array): Uint8Array => {
+        const result = new Uint8Array(a.length + b.length);
+        result.set(a);
+        result.set(b, a.length);
+        return result;
+    };
+
+    const encodeSingleValue = (value: Value): Uint8Array => {
+        const type = new Uint8Array(1);
+        let data: Uint8Array;
+        if (typeof value == 'number') {
+            type[0] = TYPE_NUMBER;
+            data = new Uint8Array(8);
+            new DataView(data.buffer).setFloat64(0, value);
+        } else if (typeof value == 'string') {
+            type[0] = TYPE_STRING;
+            const bytes = utf8.encode(value);
+            const length = new Uint8Array(4);
+            new DataView(length.buffer).setUint32(0, bytes.length);
+            data = concat(length, bytes);
+        } else {
+            throw new Error('Invalid value type.');
+        }
+        return concat(type, data);
+    };
+
+    export const encodeTuple = (values: Value[]): Uint8Array => {
+        const length = new Uint8Array(4);
+        new DataView(length.buffer).setUint32(0, values.length);
+        let result = length;
+        for (const value of values) {
+            result = concat(result, encodeSingleValue(value));
+        }
+        return result;
+    };
+}
 
 export class StorageEncryptionKey {
     readonly #keyIdPromise: Promise<string>;
@@ -38,12 +82,12 @@ export class StorageEncryptionKey {
 
     public async getHashedStoreName(storeName: KeyValueStoreName): Promise<string> {
         const hmac = await this.#indexHmacKeyPromise;
-        return await hmac.signStringBase64(JSON.stringify([storeName]));
+        return await hmac.signBytesBase64(TupleEncoder.encodeTuple([storeName]));
     }
 
     public async getHashedStoreKey(storeName: KeyValueStoreName, key: KeyValueStoreKey): Promise<string> {
         const hmac = await this.#indexHmacKeyPromise;
-        return await hmac.signStringBase64(JSON.stringify([storeName, key]));
+        return await hmac.signBytesBase64(TupleEncoder.encodeTuple([storeName, key]));
     }
 }
 
